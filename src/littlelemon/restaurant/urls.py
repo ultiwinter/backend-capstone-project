8986abbc2a66bdeb1791a8d1577e279a9f390475
Ahmed Sheta <ahmedhesham73@gmail.com>
2026-01-01 16:18:35 +0100

    add read me

--- src/littlelemon/restaurant/urls.py
+++ src/littlelemon/restaurant/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin 
 from django.urls import path 
 from .views import index, MenuItemsView, SingleMenuItemView, menu_page, booking_page, about_page
-from rest_framework.authtoken.views import obtain_auth_token # step 8 Securing the table booking API - Step 4 import obtain_auth_token
-  
+ 
 urlpatterns = [ 
     path('', index, name='home'), # Step 2 Django Static Routes - Step 5 Add URL Route for index view
     path('menu/', MenuItemsView.as_view()), # step 5 Set up the menu API - Step 5 add URL patterns for Menu API
